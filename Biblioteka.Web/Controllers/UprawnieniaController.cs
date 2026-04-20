5aafa3e526533d9026f3e10e473d598b96580423
Emil Górski <emil.gorski6@gmail.com>
2026-04-20 08:37:40 +0200

    logika pod wyszukiwanie użytkowników z wieloma uprawnieniami

--- Biblioteka.Web/Controllers/UprawnieniaController.cs
+++ Biblioteka.Web/Controllers/UprawnieniaController.cs
@@ -2,22 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Biblioteka.Web.Models;
 using Biblioteka.Web.Data;
+using Biblioteka.Web.Data.Entities; // Dodane dla dostępu do encji Uzytkownik
 using System.Linq;
 
 namespace Biblioteka.Web.Controllers
 {
     public class UprawnieniaController : Controller
     {
-        private readonly BibliotekaDbContext _context; // Zmiana nazwy dla spójności
+        private readonly BibliotekaDbContext _context;
 
         public UprawnieniaController(BibliotekaDbContext context)
         {
             _context = context;
         }
 
-        public IActionResult Index()
+        // Zaktualizowana metoda Index obsługująca filtrowanie
+        public IActionResult Index(string[] selectedRoles)
         {
-            var listaDoWyswietlenia = _context.Uprawnienia
+            // 1. Pobieramy listę wszystkich uprawnień, aby wyświetlić "chipsy" w widoku
+            var listaUprawnien = _context.Uprawnienia
                 .Select(uprawnienie => new UprawnienieItemViewModel
                 {
                     Id = uprawnienie.Id,
@@ -26,24 +29,51 @@ namespace Biblioteka.Web.Controllers
                     LiczbaUzytkownikow = uprawnienie.Uzytkownicy.Count(u => !u.CzyZapomniany)
                 }).ToList();
 
-            return View(listaDoWyswietlenia);
+            // 2. Inicjujemy zapytanie o użytkowników
+            var userQuery = _context.Uzytkownicy
+                .Include(u => u.Uprawnienia)
+                .Where(u => !u.CzyZapomniany)
+                .AsQueryable();
+
+            // 3. Logika filtrowania (AND) - Użytkownik musi posiadać KAŻDĄ z wybranych ról
+            if (selectedRoles != null && selectedRoles.Length > 0)
+            {
+                foreach (var role in selectedRoles)
+                {
+                    // Nakładamy kolejne filtry - każdy musi zostać spełniony
+                    userQuery = userQuery.Where(u => u.Uprawnienia.Any(r => r.Nazwa == role));
+                }
+
+                // Pobieramy wyniki tylko jeśli wybrano jakieś role
+                ViewBag.FilteredUsers = userQuery.ToList();
+            }
+            else
+            {
+                // Jeśli nic nie zaznaczono, przekazujemy pustą listę (lub wszystkich, zależnie od preferencji)
+                ViewBag.FilteredUsers = new List<Uzytkownik>();
+            }
+
+            // Przekazujemy wybrane role z powrotem, aby checkboxy zostały zaznaczone po odświeżeniu
+            ViewBag.SelectedRoles = selectedRoles ?? new string[0];
+
+            return View(listaUprawnien);
         }
 
         public IActionResult Szczegoly(int id)
         {
-            // 1. Pobieramy Uprawnienie i jego użytkowników (podstawowy Include działa w SQLite)
+            // Pobieramy Uprawnienie i jego użytkowników
             var uprawnienie = _context.Uprawnienia
                 .Include(p => p.Uzytkownicy)
                 .FirstOrDefault(p => p.Id == id);
 
             if (uprawnienie == null) return NotFound();
 
-            // 2. Filtrowanie i mapowanie robimy w pamięci (Memory), co rozwiązuje problem SQLite
+            // Filtrowanie i mapowanie w pamięci (Memory) dla SQLite
             var model = new UprawnienieSzczegolyViewModel
             {
                 NazwaUprawnienia = uprawnienie.Nazwa.ToUpper(),
                 Uzytkownicy = uprawnienie.Uzytkownicy
-                    .Where(u => !u.CzyZapomniany) // Filtrujemy w C#
+                    .Where(u => !u.CzyZapomniany)
                     .Select(u => new UzytkownikZUprawnieniem
                     {
                         Login = u.Login,
@@ -52,7 +82,6 @@ namespace Biblioteka.Web.Controllers
                     }).ToList()
             };
 
-            // Zwracamy widok
             return View("uprawnienia", model);
         }
     }
